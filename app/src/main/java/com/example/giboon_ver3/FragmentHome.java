bda2ad414a8277321e213d3d6f731c3fa4f9d292
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2020-10-29 00:25:28 +0900

    201029 ChangeCampaignActivity.java FragmentHome.java dialog_btn_custom.xml activity_change_campaign.xml fragment_home.xml AndroidManifest.xml 수정

--- app/src/main/java/com/example/giboon_ver3/FragmentHome.java
+++ app/src/main/java/com/example/giboon_ver3/FragmentHome.java
@@ -1,16 +1,59 @@
 package com.example.giboon_ver3;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SlidingDrawer;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FragmentHome extends Fragment {
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private String manager = "zolpzoluv@gmail.com";
+    private static final String TAG = "ChangeCampaign";
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        final SlidingDrawer slidingDrawer = (SlidingDrawer)view.findViewById(R.id.slidingdrawer);
+        final Button handle = (Button)view.findViewById(R.id.handle);
+        slidingDrawer.setOnDrawerOpenListener(new SlidingDrawer.OnDrawerOpenListener() {
+            public void onDrawerOpened() {
+                handle.setText("▼");
+            }
+        });
+        slidingDrawer.setOnDrawerCloseListener(new SlidingDrawer.OnDrawerCloseListener() {
+            public void onDrawerClosed() {
+                handle.setText("△");
+            }
+        });
+
+        final Button slidingButton = (Button)view.findViewById(R.id.slidingButton);
+        if(mAuth.getCurrentUser().getEmail().equals(manager)){
+            slidingButton.setText("새 캠페인 등록하기");
+        }
+
+        if(slidingButton.getText().toString().equals("새 캠페인 등록하기")){
+            slidingButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getActivity(), ChangeCampaignActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+        return view;
     }
 }
